4e7cbc94dab6a7af4b30fa1db0383c2b631dd9c7
browna38 <ajbrown000@gmail.com>
2020-08-30 20:55:18 -0500

    added gitignore

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("home");
+})
+
+let port = process.env.PORT;
+if(port ==null || port "") {
+    port = 3000;
+}
+
+app.listen(port, function(){
+    console.log("Server Started Successfully");
+});
