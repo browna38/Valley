a9b91652774d5777ae31fe64a47304ccf53e0e90
browna38 <ajbrown000@gmail.com>
2020-09-22 20:52:25 -0500

    fix views

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.set('views', path.join(__dirname, './Views'));
+app.set('views', path.join(__dirname, './views'));
 
 app.get('/', function(req, res){
     res.render('home');
